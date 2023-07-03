78e0df2aebed61587efa9e6d48135f3a80f39c74
Vito <vitomanz@hotmail.co.uk>
2023-07-03 16:43:19 +0100

    dropdown menu fades when mouseover is outside of target

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
- 
+const userBtn = document.getElementById("you-btn");
+
+function openUserMenu() {
+  const menu = document.getElementById("you-menu");
+  menu.classList.remove("hidden");
+  menu.classList.add("visible");
+}
+userBtn.addEventListener("click", openUserMenu);
+
+////
+const dropdown = document.getElementById("options-btn");
+const dropdownMenu = document.getElementById("options-list");
+
+dropdown.addEventListener("click", function (ev) {
+  console.log("dropdown btn clicked");
+  dropdownMenu.style.display = "block";
+});
+
+//dropdown event listener
+
+document.body.addEventListener("mouseover", function (ev) {
+  if (!dropdownMenu.contains(ev.target) && !dropdown.contains(ev.target)) {
+    //close list
+    dropdownMenu.style.display = "none";
+  }
+});
